f5d4453ac3c0f0f563cfacd2ea0b1f34d34423df
John Garrett <jacgarrett18@gmail.com>
2020-01-15 12:21:17 +0100

    add ece2036 templating

---
+++ touch-cpp-template.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+now="$(date +'%b %d')"
+read -p "enter file name: " filename
+read -p "enter assignment name: " assignment_name
+read -p "enter assignment purpose: " assignment_purpose
+
+echo "/*\n * $filename.cpp\n * $assignment_name\n * $now JOHN GARRETT\n *\n * $assignment_purpose\n */" > $filename.cpp
