c4791038eb1c3c42b35d8a25a6ec66f80fcdf1b3
John Garrett <jacgarrett18@gmail.com>
2020-01-15 12:32:10 +0100

    mitigate POSIX compliance issues

--- touch-cpp-template.sh
+++ touch-cpp-template.sh
@@ -5,4 +5,4 @@ read -p "enter file name: " filename
 read -p "enter assignment name: " assignment_name
 read -p "enter assignment purpose: " assignment_purpose
 
-echo "/*\n * $filename.cpp\n * $assignment_name\n * $now JOHN GARRETT\n *\n * $assignment_purpose\n */" > $filename.cpp
+printf "/*\n * $filename.cpp\n * $assignment_name\n * $now JOHN GARRETT\n *\n * $assignment_purpose\n */" > $filename.cpp
